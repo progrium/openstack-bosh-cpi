d9e36417e81035fd3275083cf22b427dc7cf6051
Ferran Rodenas <frodenas@gmail.com>
2012-05-07 00:50:44 +0200

    Fix validate_deployment unit test

--- spec/unit/validate_deployment_spec.rb
+++ spec/unit/validate_deployment_spec.rb
@@ -5,12 +5,10 @@ require File.expand_path("../../spec_helper", __FILE__)
 describe Bosh::OpenStackCloud::Cloud do
 
   it "doesn't implement `validate_deployment'" do
-    cloud = make_cloud
+    cloud = mock_cloud
     expect {
       cloud.validate_deployment({}, {})
-    }.to raise_error(Bosh::Clouds::NotImplemented,
-                     "`validate_deployment' is not implemented "\
-                     "by Bosh::OpenStackCloud::Cloud")
+    }.to raise_error(Bosh::Clouds::NotImplemented, "`validate_deployment' is not implemented by Bosh::OpenStackCloud::Cloud")
   end
 
 end
